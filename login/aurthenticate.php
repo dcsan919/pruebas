552b8a8cd1138fedf2af1d74d53bf3c75aa576a0
David Santiago Cen Pool <cendavid6@gmail.com>
2025-12-01 16:10:34 -0600

    Login funcional

---
+++ login/aurthenticate.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(!isset($_POST['email']) || !isset($_POST['password'])) {
+        echo " - Faltan campos de entrada.";
+        exit;
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if($email === 'cendavid919@gmail.com' && $password === '12345') {
+        echo " - Autenticación exitosa.";
+    } else {
+        echo " - Credenciales incorrectas.";
+    }
+}
+
+?>
